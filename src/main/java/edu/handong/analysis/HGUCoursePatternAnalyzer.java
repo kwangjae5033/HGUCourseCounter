b62efc10a9373ef459843529c678173719dcffbc
kwangjae <kwangjae5033@gmail.com>
2019-05-29 09:42:04 +0900

    Adding external LIbrary for CSV

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,9 +1,17 @@
 package edu.handong.analysis;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
@@ -13,6 +21,10 @@ import edu.handong.analysis.utils.Utils;
 public class HGUCoursePatternAnalyzer {
 
 	private HashMap<String,Student> students;
+	String path;
+	boolean verbose;
+    boolean help;
+	boolean fullpath;
 	
 	/**
 	 * This method runs our analysis logic to save the number courses taken by each student per semester in a result file.
@@ -21,6 +33,36 @@ public class HGUCoursePatternAnalyzer {
 	 */
 	public void run(String[] args) {
 		
+		Options options = createOptions();
+	      
+	    if(parseOptions(options, args)){
+	         if (help){
+	            printHelp(options);
+	            return;
+	         }
+	         // path is required (necessary) data so no need to have a branch.
+	         System.out.println("You provided \"" + path + "\" as the value of the option p");
+	         
+	         // TODO show the number of files in the path
+	        
+	         File file = new File(path);
+	         System.out.println(file.listFiles().length);     
+	         
+	         if(verbose) {
+	            // TODO list all files in the path
+	            File folder = new File(path);
+	            listFilesForFolder(folder);
+	            System.out.println("Your program is terminated. (This message is shown because you turned on -v option!");
+
+	         }
+
+	         if(fullpath) {
+	            File file2 = new File(path);
+	            System.out.println("Fullpath: " + file2.getAbsolutePath());
+	         }
+	         
+	      }
+	    // HW5 skeleton from here 
 		try {
 			// when there are not enough arguments from CLI, it throws the NotEnoughArgmentException which must be defined by you.
 			if(args.length<2)
@@ -105,6 +147,73 @@ public class HGUCoursePatternAnalyzer {
 		
 	}//end of countNumberOfCoursesTakenInEachSemester
 	
+	 
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+		
+		try {
+			
+			CommandLine cmd = parser.parse(options, args);
+
+	        path = cmd.getOptionValue("p");
+	        verbose = cmd.hasOption("v");
+	        help = cmd.hasOption("h");
+	        fullpath = cmd.hasOption("f");
+
+	      } catch (Exception e) {
+	        printHelp(options);
+	        return false;
+	      }
+
+	      return true;
+
+	}
 	
+	
+	// Definition Stage
+	private Options createOptions() {
+		
+		Options options = new Options();
+		
+		// add options by using OptionBuilder
+	    options.addOption(Option.builder("p").longOpt("path")
+	           .desc("Set a path of a directory or a file to display")
+	           .hasArg()
+	           .argName("Path name to display")
+	           .required()
+	           .build());
+	    
+	    // add options by using OptionBuilder
+	    options.addOption(Option.builder("v").longOpt("verbose")
+	           .desc("Display detailed messages!")
+	           //.hasArg()     // this option is intended not to have an option value but just an option
+	           .argName("verbose option")
+	           //.required() // this is an optional option. So disabled required().
+	           .build());
+	      
+	    // add options by using OptionBuilder
+	    options.addOption(Option.builder("h").longOpt("help")
+	           .desc("Help")
+               .build());
+	      
+	    // add options by using OptionBuilder
+	    options.addOption(Option.builder("f").longOpt("fullpath")
+	           .desc("Set a fullpath of a directory or a file to display")
+	           .build());
+
+	      return options;
+	
+	}//end createOptions function
+	
+	 
+	private void printHelp(Options options) {
+		// automatically generate the help statement
+	    HelpFormatter formatter = new HelpFormatter();
+	    String header = "CLI test program";
+	    String footer ="\nPlease report issues at https://github.com/lifove/CLIExample/issues";
+	    formatter.printHelp("CLIExample", header, options, footer, true);
+	
+	}
+
 }//end of class HGUCourseCounter 
 
